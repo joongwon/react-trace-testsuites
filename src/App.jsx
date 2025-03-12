e39f4b07b8569fad50c3b782feed8743ff848ebe
joongwon <winterdawnlight@snu.ac.kr>
2025-03-12 10:47:16 +0900

    setup tests

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,108 @@ import React from "react";
 import { useState, useEffect } from "react";
 import print from "./print";
 
+//let C _ =
+//  let (setter, setSetter) = useState^setter () in
+//  let (render, setRender) = useState^render 0 in
+//  (if render < 3 then setRender(fun r -> r + 1));
+//
+//  (if (setter <> ()) && (render < 3) then setter(fun _ -> 1));
+//  let (value, setValue) = useState^value 0 in
+//  (if (setter = ()) && (render < 3) then setSetter(fun _ -> setValue));
+//  print render;
+//  print value;
+//  value
+
+//function App() {
+//  const [setter, setSetter] = useState();
+//  const [render, setRender] = useState(0);
+//  if (render < 3) {
+//    setRender((r) => r + 1);
+//  }
+//
+//  if (setter && render < 3) {
+//    setter(() => 1);
+//  }
+//
+//  const [value, setValue] = useState(0);
+//  if (!setter && render < 3) {
+//    setSetter(() => setValue);
+//  }
+//  console.log("r", render);
+//  console.log("v", value);
+//  return value;
+//}
+
+//let App _ =
+//  print("start App()");
+//  let (data, setData) = useState(
+//    print("init");
+//    0
+//  ) in
+//  useEffect(print("effect"));
+//  print("end App()");
+//  [
+//    fun _ -> (
+//      print("start onClick");
+//      setData(fun d -> (
+//        print("start set1: d =");
+//        print(d);
+//        setData(fun d -> (
+//          print("start set2: d =");
+//          print(d);
+//          print("end set2: return");
+//          print(d - 1);
+//          d - 1
+//        ));
+//        print("end set1: return");
+//        print(d + 1);
+//        d + 1
+//      ));
+//      print("end onClick")
+//    ),
+//    data
+//  ]
+//;;
+//App ()
+
+//function App() {
+//  console.log("start App()");
+//  const [data, setData] = useState(() => {
+//    console.log("init");
+//    return 0;
+//  });
+//  useEffect(() => {
+//    console.log("effect");
+//  });
+//  console.log("end App()");
+//  return <button onClick={
+//    () => {
+//      console.log("start onClick");
+//      setData((d) => {
+//        console.log("start set1: d =", d);
+//        setData((d) => {
+//          console.log("start set2: d =", d);
+//          console.log("end set2: return", d - 1);
+//          return d - 1;
+//        });
+//        console.log("end set1: return", d + 1);
+//        return d + 1;
+//      });
+//      console.log("end onClick");
+//    }
+//  } />;
+//}
+
+
 function App() {
-  print("App.jsx");
-  const [count, setCount] = useState(0);
+  print("Couter");
+  const [s, setS] = useState(0);
   useEffect(() => {
     print("Effect");
+    setS((s) => {if (s % 2 === 0) {print("Update"); return s + 1} else {return s}});
   });
-  return (
-    <>
-      <div role="div">{count}</div>
-      <button onClick={() => setCount((c) => c + 1)}>click</button>
-    </>
-  );
+  print("Return");
+  return <> {s} </>;
 }
 
 export default App;
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,25 +1,177 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import App from "./App";
-import * as Print from "./print";
 
-const printMock = jest.fn();
-Print.default = printMock;
-function expectPrintCalls(calls) {
-  expect(printMock.mock.calls).toEqual(calls.map((c) => [c]));
+const print = jest.fn();
+function expectPrintCalls(...calls) {
+  expect(print.mock.calls.map((c) => c.map((x) => `${x}`))).toEqual(
+    calls.map((c) => [`${c}`]),
+  );
 }
 
-test("loads and displays greeting", async () => {
-  // ARRANGE
-  render(<App />);
+const doneMarker = "__done__";
+const clickMarker = "__click__";
+function DoneButton() {
+  const [done, setDone] = useState(false);
+  return (
+    <button onClick={() => setDone(true)} disabled={done}>
+      {done ? doneMarker : clickMarker}
+    </button>
+  );
+}
 
-  // ACT
-  await userEvent.click(screen.getByText("click"));
-  await screen.findByRole("div");
+async function clickDone() {
+  userEvent.click(screen.getByText(clickMarker));
+  await screen.findByText(doneMarker);
+}
 
-  // ASSERT
-  expect(screen.getByRole("div")).toHaveTextContent("1");
-  expectPrintCalls(["App.jsx", "Effect", "App.jsx", "Effect"]);
+function testRender(C) {
+  render(
+    <>
+      <C />
+      <DoneButton />
+    </>,
+  );
+}
+
+test("Flush effect queue on retry", async () => {
+  function C() {
+    const [s, setS] = useState(0);
+    print("C");
+    if (s === 0) {
+      setS((s) => s + 1);
+    }
+    useEffect(() => {
+      print("useEffect");
+      setS((s) => 42);
+    });
+    return s;
+  }
+
+  testRender(C);
+
+  await clickDone();
+
+  expectPrintCalls("C", "C", "useEffect", "C", "useEffect", "C");
+});
+
+test("Flush effect queue on retry", async () => {
+  function C() {
+    useEffect(() => {
+      print("useEffect");
+      setS((s) => 42);
+    });
+    const [s, setS] = useState(0);
+    print("C");
+    if (s === 0) {
+      setS((s) => s + 1);
+    }
+    return s;
+  }
+
+  testRender(C);
+
+  await clickDone();
+
+  expectPrintCalls("C", "C", "useEffect", "C", "useEffect", "C");
+});
+
+test("Component is called once more after set state in effect", async () => {
+  function C() {
+    const [s, setS] = useState(0);
+    useEffect(() => {
+      print("useEffect");
+      setS((s) => 42);
+    });
+    print("C");
+    return s;
+  }
+
+  testRender(C);
+
+  await clickDone();
+
+  expectPrintCalls("C", "useEffect", "C", "useEffect", "C");
+});
+
+test("Idle child's effects are run when parent re-renders", async () => {
+  function C(x) {
+    useEffect(() => {
+      print("C");
+    });
+    return "C";
+  }
+  function D() {
+    const [s, setS] = useState(0);
+    useEffect(() => {
+      setS(() => 42);
+    });
+    return <C s={0} />;
+  }
+
+  testRender(D);
+  await clickDone();
+  expectPrintCalls("C", "C");
+});
+
+test("Nested view render order", async () => {
+  function C({ x }) {
+    useEffect(() => {
+      print(x);
+    });
+    return x;
+  }
+  function D() {
+    const [x, setX] = useState(0);
+    useEffect(() => {
+      setX(() => 42);
+    });
+    useEffect(() => {
+      print("D");
+    });
+    return (
+      <>
+        <C x={0} />
+        <>
+          <C x={1} />
+          <C x={2} />
+        </>
+      </>
+    );
+  }
+  function E() {
+    useEffect(() => {
+      print("E");
+    });
+    return (
+      <>
+        <D />
+        <C x="3" />
+      </>
+    );
+  }
+
+  testRender(E);
+  await clickDone();
+  expectPrintCalls("0", "1", "2", "D", "3", "E", "0", "1", "2", "D");
+});
+
+test("Event handler prints", async () => {
+  function C() {
+    return (
+      <>
+        <button onClick={() => print("0")}>0</button>
+        <button onClick={() => print("1")}>1</button>
+      </>
+    );
+  }
+
+  testRender(C);
+  userEvent.click(screen.getByText("0"));
+  userEvent.click(screen.getByText("1"));
+  await clickDone();
+  expectPrintCalls("0", "1");
 });
